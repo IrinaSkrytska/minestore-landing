909f489cb36db7a9a69da389c2dcfd7a59aa321a
arcticmaticwebsite <93982819+IrinaSkrytska@users.noreply.github.com>
2024-07-30 23:44:16 +0300

    faq section fixes

--- features.js
+++ features.js
@@ -2,62 +2,194 @@ document.addEventListener("DOMContentLoaded", function () {
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
-        if (
-          entry.target.classList.contains("categories-thumb") ||
-          entry.target.classList.contains("option") ||
-          entry.target.classList.contains("reviews-section") ||
-          entry.target.classList.contains("pricing-section")
-        ) {
-          entry.target.classList.add("visible-flex");
-        } else if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-        } else {
-          entry.target.classList.remove("visible");
+        const currentScrollTop =
+          window.scrollY || document.documentElement.scrollTop;
+
+        // Check if scrolling downwards
+        if (currentScrollTop > lastScrollTop) {
+          if (
+            entry.target.classList.contains("categories-thumb") ||
+            entry.target.classList.contains("option") ||
+            entry.target.classList.contains("reviews-section") ||
+            entry.target.classList.contains("pricing-section") ||
+            entry.target.classList.contains("benefit-list") ||
+            entry.target.classList.contains("benefit-descriptions-list")
+          ) {
+            entry.target.classList.add("visible-flex");
+          } else if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          } else {
+            entry.target.classList.remove("visible");
+          }
         }
+
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
       });
     },
     {
       threshold: 0.1,
+      rootMargin: "0px 0px 0px 0px",
     }
   );
 
-  document.querySelectorAll(".features-info-thumb").forEach((element) => {
-    observer.observe(element);
-  });
-  document.querySelectorAll(".more-sub-features-thumb").forEach((element) => {
-    observer.observe(element);
-  });
-  document.querySelectorAll(".faq-item").forEach((element) => {
-    observer.observe(element);
+  // List of selectors to be observed
+  const selectors = [
+    ".features-info-thumb",
+    ".benefit-line",
+    ".benefits-thumb",
+    ".more-features-thumb",
+    ".more-title",
+    ".more-subtitle",
+    ".more-features-thumb",
+    ".antifraud-thumb",
+    ".categories-options-list",
+    ".categories-section",
+    ".reviews-section",
+    ".pricing-section",
+    ".footer",
+  ];
+
+  // Observe all elements matching the selectors
+  selectors.forEach((selector) => {
+    document.querySelectorAll(selector).forEach((element) => {
+      observer.observe(element);
+    });
   });
 
-  document.querySelectorAll(".categories-text-thumb").forEach((element) => {
-    observer.observe(element);
+  // FAQ
+
+  const accordion = document.getElementById("accordion");
+  const items = accordion.querySelectorAll(".accordion-item");
+
+  items.forEach((item) => {
+    const link = item.querySelector(".link");
+    const arrowIcon = link.querySelector(".arrow-icon");
+    const submenu = item.querySelector(".submenu");
+
+    // Function to toggle the accordion item
+    const toggleAccordion = () => {
+      const isOpen = item.classList.contains("open");
+
+      // Toggle the visibility and rotation
+      submenu.style.maxHeight = isOpen ? "0px" : submenu.scrollHeight + "px";
+      submenu.style.opacity = isOpen ? "0" : "1";
+      link.parentElement.classList.toggle("open", !isOpen);
+
+      // Close other items if not allowing multiple open
+      if (!accordion.dataset.multiple) {
+        items.forEach((otherItem) => {
+          if (otherItem !== item) {
+            const otherSubmenu = otherItem.querySelector(".submenu");
+            otherSubmenu.style.maxHeight = "0px";
+            otherSubmenu.style.opacity = "0";
+            otherItem.classList.remove("open");
+          }
+        });
+      }
+    };
+
+    // Add click event to the link and arrow icon
+    link.addEventListener("click", toggleAccordion);
+    arrowIcon.addEventListener("click", (e) => {
+      e.stopPropagation(); // Prevent click event from bubbling up
+      toggleAccordion();
+    });
   });
 
-  document.querySelectorAll(".more-thumb").forEach((element) => {
-    observer.observe(element);
-  });
+  // Slider functionality
 
-  document.querySelectorAll(".option").forEach((element) => {
-    observer.observe(element);
-  });
+  const desktopMinWidth = 1200;
+  const scrollOffset = 700; // Offset to scroll a little beyond the current view
 
-  document.querySelectorAll(".categories-thumb").forEach((element) => {
-    observer.observe(element);
-  });
+  if (window.innerWidth >= desktopMinWidth) {
+    const sliderContainer = document.querySelector(".slider-container");
+    const titleItems = document.querySelectorAll(".benefit-title-item");
+    const descriptionItems = document.querySelectorAll(
+      ".benefit-description-item"
+    );
 
-  document.querySelectorAll(".reviews-section").forEach((element) => {
-    observer.observe(element);
-  });
+    let isPageScrolling = false;
 
-  document.querySelectorAll(".pricing-section").forEach((element) => {
-    observer.observe(element);
-  });
+    // Function to handle the scroll effect
+    function handleScroll() {
+      const maxScrollLeft =
+        sliderContainer.scrollWidth - sliderContainer.clientWidth;
+      const scrollLeft = sliderContainer.scrollLeft;
 
-  document.querySelectorAll(".footer-thumb").forEach((element) => {
-    observer.observe(element);
-  });
+      // Determine if we're at the end or start
+      if (scrollLeft >= maxScrollLeft - 5) {
+        // Threshold for the end
+        if (!isPageScrolling) {
+          isPageScrolling = true;
+          window.scrollBy({
+            top: scrollOffset,
+            behavior: "smooth",
+          });
+          setTimeout(() => {
+            isPageScrolling = false;
+          }, 1000); // Adjust timeout as needed
+        }
+      } else if (scrollLeft <= 5) {
+        // Threshold for the start
+        if (!isPageScrolling) {
+          isPageScrolling = true;
+          window.scrollBy({
+            top: -scrollOffset,
+            behavior: "smooth",
+          });
+          setTimeout(() => {
+            isPageScrolling = false;
+          }, 1000); // Adjust timeout as needed
+        }
+      }
+    }
+
+    // Handle slider container scroll
+    sliderContainer.addEventListener("scroll", handleScroll);
+
+    // Handle wheel event for horizontal scrolling
+    sliderContainer.addEventListener("wheel", (event) => {
+      event.preventDefault();
+      sliderContainer.scrollBy({
+        left: event.deltaY < 0 ? -220 : 220,
+        behavior: "smooth",
+      });
+    });
+
+    // Activate item and handle page scroll
+    function activateItem(index) {
+      titleItems.forEach((item) => {
+        item.classList.remove("highlighted");
+      });
+
+      descriptionItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      if (descriptionItems[index]) {
+        descriptionItems[index].classList.add("active");
+      }
+
+      titleItems[index].classList.add("highlighted");
+    }
+
+    // Initial activation
+    activateItem(0);
+
+    // Add click event listeners to title items
+    titleItems.forEach((titleItem, index) => {
+      titleItem.addEventListener("click", () => {
+        activateItem(index);
+      });
+    });
+
+    // Optional: Add a resize event listener to recheck the window width if the user resizes the window
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < desktopMinWidth) {
+        location.reload(); // Reload to reapply the scroll functionality on resize
+      }
+    });
+  }
 
   // Benefits items click action
   const titleItems = document.querySelectorAll(".benefit-title-item");
@@ -86,41 +218,6 @@ document.addEventListener("DOMContentLoaded", function () {
   titleItems.forEach((titleItem, index) => {
     titleItem.addEventListener("click", () => {
       activateItem(index);
-    });
-  });
-
-  // FAQ
-
-  const faqItems = document.querySelectorAll(".faq-item");
-
-  faqItems.forEach((item) => {
-    const questionThumb = item.querySelector(".faq-question-thumb");
-    const answer = item.querySelector(".faq-answer");
-    const arrowIcon = item.querySelector(".arrow-icon");
-
-    questionThumb.addEventListener("click", function () {
-      // Check if the clicked item's answer is already visible
-      const isAnswerVisible = answer.style.display === "block";
-
-      // Hide all answers and remove the active class from all items
-      faqItems.forEach((otherItem) => {
-        const otherAnswer = otherItem.querySelector(".faq-answer");
-        const otherArrow = otherItem.querySelector(".arrow-icon");
-        otherItem.classList.remove("active-item");
-        otherAnswer.style.display = "none";
-        otherArrow.classList.remove("rotate");
-      });
-
-      // If the clicked answer was not visible, show it, add active class, and rotate the arrow
-      if (!isAnswerVisible) {
-        answer.style.display = "block";
-        item.classList.add("active-item");
-        arrowIcon.classList.add("rotate");
-      } else {
-        // If the answer was visible, just remove the active class and rotate class
-        item.classList.remove("active-item");
-        arrowIcon.classList.remove("rotate");
-      }
     });
   });
 
